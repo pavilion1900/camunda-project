23b405933c3e7f19cbd9bb63f3243b2b2d0c599c
SergeyKorolenko <skorolenko@alfabank.ru>
2023-06-28 00:04:30 +0300

    task3

--- src/main/java/ru/clevertec/Application.java
+++ src/main/java/ru/clevertec/Application.java
@@ -2,6 +2,7 @@ package ru.clevertec;
 
 import io.camunda.zeebe.client.ZeebeClient;
 import io.camunda.zeebe.client.api.response.DeploymentEvent;
+import io.camunda.zeebe.client.api.response.ProcessInstanceEvent;
 import io.camunda.zeebe.spring.client.EnableZeebeClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
@@ -9,6 +10,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.time.Duration;
+import java.util.Map;
 
 @SpringBootApplication
 @EnableZeebeClient
@@ -23,18 +25,23 @@ public class Application implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        //deploy
-        //check http://93.84.86.69:9081/swagger-ui/index.html#/Process/byKey_2
+        //deploy process
 //        DeploymentEvent event = client.newDeployResourceCommand()
-//                .addResourceFromClasspath("intermsg_01.bpmn")
+//                .addResourceFile("D:\\camunda\\trainer023.bpmn")
+////                .addResourceFromClasspath("intermsg_01.bpmn")
 //                .send()
 //                .join();
 //        long processDefinitionKey = event.getProcesses().get(0).getProcessDefinitionKey();
 //        System.out.println("processDefinitionKey " + processDefinitionKey);
 
+        //Проверить, что процесс задеплоин можно через REST API Operate
+        //http://93.84.86.69:9081/swagger-ui/index.html#/Process/byKey_2
+        //http://localhost:8081/swagger-ui/index.html#/Process/byKey_2
+
         //start diagram
 //        ProcessInstanceEvent event = client.newCreateInstanceCommand()
-//                .bpmnProcessId("s_korolenkoID01")
+//                .bpmnProcessId("trainerID023")
+////                .version(3)
 //                .latestVersion()
 //                .variables(Map.of("key", 123))
 //                .send().join();
@@ -43,12 +50,17 @@ public class Application implements CommandLineRunner {
 //        System.out.println("Version " + event.getVersion());
 //        System.out.println("ProcessInstanceKey " + event.getProcessInstanceKey());
 
+
+        //stop process
+        long processInstanceKey = 2251799813772745L;
+        client.newCancelInstanceCommand(processInstanceKey).send().join();
+
         //publish message
-        client.newPublishMessageCommand()
-                .messageName("intermsg_01")
-                .correlationKey("100")
-                .timeToLive(Duration.ZERO)
-                .send()
-                .join();
+//        client.newPublishMessageCommand()
+//                .messageName("intermsg_01")
+//                .correlationKey("100")
+//                .timeToLive(Duration.ZERO)
+//                .send()
+//                .join();
     }
 }
